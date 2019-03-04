d54aec6c8fa0da407b22e312a16a747c337e5aba
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-03-04 16:48:20 +0100

    update of itextpdf to 7

--- src/main/java/priceExtractor/CmdMain.java
+++ src/main/java/priceExtractor/CmdMain.java
@@ -1,14 +1,12 @@
 package priceExtractor;
 
-import com.itextpdf.text.DocumentException;
-
 import java.io.File;
 import java.io.IOException;
 
 public class CmdMain {
 	
-	public static void main(String[] args) throws IOException, DocumentException {
-		new PriceExtractor(new File("test.csv")).run();
+	public static void main(String[] args) throws IOException {
+		new PriceExtractor(new File("test.csv"), new File("export.pdf")).run();
 	}
 	
 }
--- src/main/java/priceExtractor/PriceExtractor.java
+++ src/main/java/priceExtractor/PriceExtractor.java
@@ -1,31 +1,58 @@
 package priceExtractor;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.*;
+import com.itextpdf.layout.property.UnitValue;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static com.itextpdf.layout.property.UnitValue.createPercentValue;
 
 public class PriceExtractor {
 	
-	private final File file;
+	private static final PdfFont FONT_TIMES_ROMAN;
+	private static final PdfFont FONT_TIMES_BOLD;
 	
-	public PriceExtractor(File file) {
-		this.file = file;
+	static {
+		try {
+			FONT_TIMES_ROMAN = PdfFontFactory.createFont(StandardFonts.TIMES_ROMAN);
+			FONT_TIMES_BOLD = PdfFontFactory.createFont(StandardFonts.TIMES_BOLD);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
-	public void run() throws IOException, DocumentException {
-		try (CSVReader reader = new CSVReaderBuilder(new FileReader(file))
+	private final File input;
+	private final File output;
+	private final float margin = 50;
+	
+	private final Function<String, Text> fontTitle = s -> new Text(s).setFont(FONT_TIMES_BOLD).setFontSize(36);
+	private final Function<String, Text> fontDesc = s -> new Text(s).setFont(FONT_TIMES_ROMAN).setFontSize(24);
+	
+	public PriceExtractor(File input, File output) {
+		this.input = input;
+		this.output = output;
+	}
+	
+	public void run() throws IOException {
+		try (CSVReader reader = new CSVReaderBuilder(new FileReader(input))
 				.withCSVParser(new CSVParserBuilder()
 						.withSeparator(';')
 						.build())
@@ -38,47 +65,47 @@ public class PriceExtractor {
 					.map(strings -> {
 						try {
 							return new Entry(strings);
-						} catch (IOException e) {
+						} catch (RuntimeException e) {
 							return null;
 						}
 					})
 					.filter(Objects::nonNull)
 					.collect(Collectors.toList());
 			
-			Document document = new Document();
-			PdfWriter.getInstance(document, new FileOutputStream("export.pdf"));
-			document.open();
-			
-			for (Entry entry : entries) {
-				PdfPTable table = new PdfPTable(1);
-				
-				PdfPTable block1 = new PdfPTable(1);
-				block1.addCell(entry.bezeichnung);
-				table.addCell(block1);
-				
-				PdfPTable block2 = new PdfPTable(1);
-				block2.addCell(entry.kisteInhalt);
-				block2.addCell(entry.kisteLiterpreis);
-				block2.addCell(entry.kistePreis);
-				block2.addCell(entry.kistePfand);
-				table.addCell(block2);
-				
-				PdfPTable block3 = new PdfPTable(1);
-				block3.addCell(entry.flascheInhalt);
-				block3.addCell(entry.flaschePreis);
-				block3.addCell(entry.flaschePfand);
-				table.addCell(block3);
-				
-				PdfPTable block4 = new PdfPTable(1);
-				block4.addCell(entry.weg.name());
-				table.addCell(block4);
-				
-				document.add(table);
-				document.newPage();
+			try (PdfDocument pdf = new PdfDocument(new PdfWriter(output))) {
+				try (Document document = new Document(pdf, PageSize.A4)) {
+					Rectangle pageSize = document.getPageEffectiveArea(PageSize.A4);
+					Rectangle outer = new Rectangle(pageSize.getLeft() + margin, pageSize.getBottom() + margin, pageSize.getRight() - margin, pageSize.getTop() - margin);
+					
+					for (Entry entry : entries) {
+						
+						Table table = new Table(1);
+						table.setWidth(createPercentValue(100));
+						
+						table.addCell(createCell(outer, createPercentValue(1f / 6), fontTitle, entry.bezeichnung));
+						table.addCell(createCell(outer, createPercentValue(2f / 6), fontDesc, entry.kisteInhalt, entry.kisteLiterpreis, entry.kistePreis, entry.kistePfand));
+						table.addCell(createCell(outer, createPercentValue(2f / 6), fontDesc, entry.flascheInhalt, entry.flaschePreis, entry.flaschePfand));
+						table.addCell(createCell(outer, createPercentValue(1f / 6), fontDesc, entry.weg.name()));
+						
+						document.add(table);
+//						document.newPage();
+					}
+					
+				}
 			}
-			
-			document.close();
 		}
+	}
+	
+	private static Cell createCell(Rectangle outer, UnitValue height, Function<String, Text> font, String... text) {
+		return createCell(outer, height, Arrays.stream(text).map(font).map(Paragraph::new).toArray(IBlockElement[]::new));
+	}
+	
+	private static Cell createCell(Rectangle outer, UnitValue height, IBlockElement... elements) {
+		Cell cell = new Cell();
+		for (IBlockElement element : elements)
+			cell.add(element);
+		cell.setHeight(height);
+		return cell;
 	}
 	
 	public static class Entry {
@@ -97,24 +124,20 @@ public class PriceExtractor {
 		
 		public final Weg weg;
 		
-		public Entry(String[] input) throws IOException {
-			try {
-				arkitelNr = Integer.parseInt(input[0]);
-				bezeichnung = input[1];
-				
-				kisteInhalt = input[2];
-				kisteLiterpreis = input[3];
-				kistePreis = input[4];
-				kistePfand = input[5];
-				
-				flascheInhalt = input[6];
-				flaschePreis = input[7];
-				flaschePfand = input[8];
-				
-				weg = Weg.parse(input[9]);
-			} catch (NumberFormatException e) {
-				throw new IOException(e);
-			}
+		public Entry(String[] input) {
+			arkitelNr = Integer.parseInt(input[0]);
+			bezeichnung = input[1];
+			
+			kisteInhalt = input[2];
+			kisteLiterpreis = input[3];
+			kistePreis = input[4];
+			kistePfand = input[5];
+			
+			flascheInhalt = input[6];
+			flaschePreis = input[7];
+			flaschePfand = input[8];
+			
+			weg = Weg.parse(input[9]);
 		}
 		
 		@Override
